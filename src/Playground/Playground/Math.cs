cff1f3731221a44acafce5e5f01a3e698c59cc1e
Simon <simon@familie-vollert.de>
2022-07-22 13:54:01 +0200

    Ermitteln von Primzahlen

--- src/Playground/Playground/Math.cs
+++ src/Playground/Playground/Math.cs
@@ -8,10 +8,105 @@ namespace Playground
 {
     internal static class Math
     {
+        /// <summary>
+        /// Ersatz für %
+        /// </summary>
+        /// <param name="wert">wert % ...</param>
+        /// <param name="divisor">... % divisor</param>
+        /// <returns>Restwert</returns>
         public static int Rest(int wert, int divisor)
         {
             int i = wert / divisor;
             return wert - (divisor * i);
+        }
+
+        /// <summary>
+        /// Berechnet die Primzahlen bis zu einer stimmer Zahl
+        /// </summary>
+        /// <param name="primzahlenBis">Zahl bis zu dem die Primzahlen berechnet werden</param>
+        /// <returns>Arrey mit allen Primzahlen</returns>
+        public static int[] SiebDesEratosthenes(int primzahlenBis)
+        {
+            // Array mit allen Zahlen erstellen
+            var zahlen = new int[primzahlenBis + 1];
+            for (int i = 0; i <= primzahlenBis; i++)
+            {
+                if (i < 2)
+                    zahlen[i] = 0;
+                else
+                    zahlen[i] = 1;
+            }
+
+            // Primzahlen erkennen und (primzahl als 1 sonst als 0) speichern
+            for (int i = 2; i < primzahlenBis; i++)
+            {
+                // Wenn die Zahl nicht schon als NICHT Primzahl makiert ist
+                if (zahlen[i] != 0)
+                {
+                    if (CheckIfPrimzahl(i) != true)
+                    {
+                        zahlen[i] = 0;
+                        // Vielfaches der (nicht Prim)Zahl aussortieren
+                        int c = i * 2;
+                        for (int multiplikator = 2; c <= primzahlenBis; c = multiplikator * i)
+                        {
+                            zahlen[multiplikator * i] = 0;
+                            multiplikator++;
+                        }
+                    }
+                }
+            }
+
+            /// Primzahlen Speichern und ausgeben
+            // Primzahlen zählen
+            int counter = 0;
+            for (int i = 0; i < primzahlenBis; i++)
+            {
+                if (zahlen[i] == 1)
+                    counter++;
+            }
+            // Primzahlen im Arrey speichern
+            var primzahlen = new int[counter];
+            int number = 0;
+            for (int i = 0; i < primzahlenBis; i++)
+            {
+                if (zahlen[i] == 1)
+                {
+                    primzahlen[number] = i;
+                    number++;
+                }
+            }
+            return primzahlen;
+        }
+
+        /// <summary>
+        /// Überprüft ob eine Zahl eine Primzahl ist
+        /// </summary>
+        /// <param name="Zahl">zu überprüfende Zahl</param>
+        /// <returns>true/false</returns>
+        public static bool CheckIfPrimzahl(int Zahl)
+        {
+            bool primzahl = true;
+            for (int i = 2; i < Zahl; i++)
+            {
+                if ((Zahl % i) == 0)
+                    primzahl = false;
+            }
+            return primzahl;
+        }
+
+        /// <summary>
+        /// Anzeige zu "SiebDesEratosthenes"
+        /// </summary>
+        /// <param name="primzahlenBis">Zahlbis zu dem die Primzahlen berechnet werden</param>
+        public static void DisplayPrimzahlen(int primzahlenBis)
+        {
+            var primzahlen = Math.SiebDesEratosthenes(primzahlenBis);
+            // Primzahlen ausgeben
+            Console.WriteLine($"Primzahlen von 1 bis {primzahlenBis}:");
+            for (int i = 0; i < primzahlen.Length; i++)
+                Console.WriteLine($"{i + 1}: {primzahlen[i]}");
+            Console.ReadLine();
         }
     }
 }
--- src/Playground/Playground/Program.cs
+++ src/Playground/Playground/Program.cs
@@ -6,30 +6,7 @@ namespace Playground
     {
         public static void Main()
         {
-
-
-            for (var wert = 1; wert < 500; wert++)
-            { 
-                for (var divisor = 3; divisor < 15; divisor++)
-                {
-
-                    var ergebnis = wert % divisor;
-                    var unserErgebnis = Math.Rest(wert, divisor);
-
-                    if (ergebnis != unserErgebnis)
-                        throw new Exception("falsches Ergebnis");
-
-                }
-            }
-
-            Console.WriteLine("alles richtig");
-            Console.ReadLine();
-
-
-        //    int accuracy = 20;
-        //    double goldenerSchnitt = Formeln.GoldenerSchnitt(accuracy);
-        //    Console.WriteLine($"Golderner Schnitt (mit Genauigkeit {accuracy}): {goldenerSchnitt}");
-        //    Console.ReadLine();
+            Math.DisplayPrimzahlen(10);
         }
     }
 }
