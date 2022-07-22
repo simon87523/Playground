c90861602d68d85d6788b104e6b8b8b0e6261bd8
Simon <simon@familie-vollert.de>
2022-07-22 11:05:39 +0200

    modulo-berechnung

---
+++ src/Playground/Playground/Math.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Playground
+{
+    internal static class Math
+    {
+        public static int Rest(int wert, int divisor)
+        {
+            int i = wert / divisor;
+            return wert - (divisor * i);
+        }
+    }
+}
--- src/Playground/Playground/Program.cs
+++ src/Playground/Playground/Program.cs
@@ -6,10 +6,30 @@ namespace Playground
     {
         public static void Main()
         {
-            int accuracy = 20;
-            double goldenerSchnitt = Formeln.GoldenerSchnitt(accuracy);
-            Console.WriteLine($"Golderner Schnitt (mit Genauigkeit {accuracy}): {goldenerSchnitt}");
+
+
+            for (var wert = 1; wert < 500; wert++)
+            { 
+                for (var divisor = 3; divisor < 15; divisor++)
+                {
+
+                    var ergebnis = wert % divisor;
+                    var unserErgebnis = Math.Rest(wert, divisor);
+
+                    if (ergebnis != unserErgebnis)
+                        throw new Exception("falsches Ergebnis");
+
+                }
+            }
+
+            Console.WriteLine("alles richtig");
             Console.ReadLine();
+
+
+        //    int accuracy = 20;
+        //    double goldenerSchnitt = Formeln.GoldenerSchnitt(accuracy);
+        //    Console.WriteLine($"Golderner Schnitt (mit Genauigkeit {accuracy}): {goldenerSchnitt}");
+        //    Console.ReadLine();
         }
     }
 }
